f9c3e43cb129dc34b10b85ea7451300cff0af7c4
mariusrueve <marius.rueve@live.de>
2020-06-08 10:24:34 +0200

    Updated shiny app

--- scrnaseq_app.R
+++ scrnaseq_app.R
@@ -10,96 +10,121 @@
 library(shiny)
 library(Seurat)
 library(ggplot2)
+library(patchwork)
 library(readxl)
 
-read_excel_allsheets <- function(filename, tibble = TRUE) {
-    # I prefer straight data.frames
-    # but if you like tidyverse tibbles (the default with read_excel)
-    # then just pass tibble = TRUE
-    sheets <- readxl::excel_sheets(filename)
-    x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
-    if(!tibble) x <- lapply(x, as.data.frame)
-    names(x) <- sheets
-    x
+param = list()
+param$col = "palevioletred"
+
+# read_excel_allsheets = function(filename, tibble = FALSE) {
+#     # I prefer straight data.frames
+#     # but if you like tidyverse tibbles (the default with read_excel)
+#     # then just pass tibble = TRUE
+#     sheets = readxl::excel_sheets(filename)
+#     x = lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+#     if(!tibble) x = lapply(x, as.data.frame)
+#     names(x) = sheets
+#     x
+# }
+#sc_markers = read_excel_allsheets("/lager2/rcug_cd/scrnaseq/projects/pbmc/results/markers.xlsx")
+
+#sc = readRDS(file = "pbmc_2020-05-27.rds")
+seurat_genes = sc@misc[["gene_lists"]]
+
+
+PlotMystyle = function(p, title=NULL, col=NULL, legend_title=NULL, legend_position=NULL) {
+    p = p + theme_light() + theme(panel.border = element_blank())
+    if (!is.null(title)) p = p + ggtitle(title) #+ theme(plot.title = element_text(hjust=0.5))
+    if (length(col) > 0) p = p + scale_fill_manual(values=col)
+    if (!is.null(legend_title)) {
+        p = p + labs(color=legend_title, fill=legend_title)
+    } else {
+        p = p + theme(legend.title = element_blank()) 
+    }
+    if (!is.null(legend_position)) p = p + theme(legend.position=legend_position)
+    return(p)
 }
-sc_markers = read_excel("/lager2/rcug_cd/scrnaseq/projects/pbmc/results/markers.xlsx")
-seurat_genes = sc_markers[["gene"]]
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui = fluidPage(
         titlePanel("Einzeldarstellungen von Genen"),
 
         sidebarPanel(
+            fileInput("rds_file","Choose Seurat file:", accept = ".rds", buttonLabel = "Browse..."),
+            selectInput("plots", "Plots:", c("FeaturePlot", "RidgePlot"), multiple = TRUE),
             selectInput("genes", "Gene:", seurat_genes, multiple = TRUE),
-            downloadButton('foo')
+            downloadButton('download_plots')
         ),
         
         mainPanel(
-            splitLayout(cellWidths = c("50%","50%"),uiOutput('out_umap'), uiOutput('out_ridge'))
+            #splitLayout(cellWidths = c("50%","50%"),uiOutput('out_feature'), uiOutput('out_ridge'))
+            tabsetPanel(type = "tabs",
+                        # tabPanel("Feature", uiOutput('ui_feature')),
+                        tabPanel("Feature", plotOutput('plot_feature')),
+                        tabPanel("Ridge", uiOutput('ui_ridge'))
+            )
         )
 )
 
-
-
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server = function(input, output) {
     
-    reactive({
-        sc_markers = read_excel_allsheets("/lager2/rcug_cd/scrnaseq/projects/pbmc/results/markers.xlsx")
-        seurat_genes = sc_markers[["gene"]]
-    })
-    
-    output$out_umap = renderUI({
-        out = list()
-        
-        if (length(input$genes)==0){return(NULL)}
-        for (i in 1:length(input$genes)){
-            out[[i]] <-  plotOutput(outputId = paste0("plot_umap",i))
-        }  
-        return(out) 
-    })
-    observe({  
-        for (i in 1:length(input$genes)){  
-            local({  #because expressions are evaluated at app init
-                ii <- i 
-                output[[paste0('plot_umap',ii)]] <- renderPlot({ 
-                        return(FeaturePlot(sc, features=input$genes[[ii]], cols=c("lightgrey", param$col), combine=FALSE))
-                })
-            })
-        }                                  
+    # # gen plot containers
+    # output$ui_feature = renderUI({
+    #     out_feature = list()
+    # 
+    #     if (length(input$genes)==0){return(NULL)}
+    #     for (i in 1:length(input$genes)){
+    #         out_feature[[i]] =  plotOutput(outputId = paste0("plot_feature",i))
+    #     }
+    #     return(out_feature)
+    # })
+    # # render plots
+    # observe({
+    #     for (i in 1:length(input$genes)){
+    #         local({  #because expressions are evaluated at app init
+    #             ii = i
+    #             output[[paste0('plot_feature',ii)]] = renderPlot({
+    #                 return(Seurat::FeaturePlot(sc, features=input$genes[[ii]], cols=c("lightgrey", param$col), combine=FALSE))
+    #             })
+    #         })
+    #     }
+    # })
+
+    out$plot_feature = renderPlot({
+      p = Seurat::FeaturePlot(sc, features=input$genes, cols=c("lightgrey", param$col), combine=FALSE)
+      names(p) = input$genes
+      for (i in features) p[[i]] = PlotMystyle(p[[i]], title=i)
+      patchwork::wrap_plots(p, ncol=2)
     })
 
-        
     #Ridge Plot
-    output$out_ridge = renderUI({
-        out = list()
+    output$ui_ridge = renderUI({
+        out_ridge = list()
         
         if (length(input$genes)==0){return(NULL)}
         for (i in 1:length(input$genes)){
-            out[[i]] <-  plotOutput(outputId = paste0("plot",i))
+            out_ridge[[i]] =  plotOutput(outputId = paste0("plot_ridge",i))
         }  
-        return(out) 
+        return(out_ridge) 
     })
     observe({  
         for (i in 1:length(input$genes)){  
             local({  #because expressions are evaluated at app init
-                ii <- i 
-                output[[paste0('plot',ii)]] <- renderPlot({ 
-                    return(RidgePlot(sc, features=input$genes[[ii]], combine=FALSE))
+                ii = i 
+                output[[paste0('plot_ridge',ii)]] = renderPlot({ 
+                    return(Seurat::RidgePlot(sc, features=input$genes[[ii]], combine=FALSE))
                 })
             })
         }                                  
     })
     
-    output$foo = downloadHandler(
-        filename = 'test.png',
+    output$downloadPlot = downloadHandler(
+        filename = function() { paste(input$dataset, '.png', sep='') },
         content = function(file) {
-            device <- function(..., width, height) {
-                grDevices::png(..., width = width, height = height,
-                               res = 300, units = "in")
-            }
-            ggsave(file, plot = output$out_ridge , device = device)
-        })
+            ggsave(file, plot = plotInput(), device = "png")
+        }
+    )
 }
 
 # Run the application 
